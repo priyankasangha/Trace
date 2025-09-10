d748e0f79b9ba5d06b6a496a974eca9097217604
Priyanka_w <priyasangha@hotmail.ca>
2025-09-10 16:15:22 -0700

    fixed import statement

--- tests/prismaTests.ts
+++ tests/prismaTests.ts
@@ -1,16 +1,16 @@
-import { PrimsaClient } from '@primsa/client';
+import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrimsaClient();
+const prisma = new PrismaClient();
 
 async function main() {
     const users = await prisma.user.findMany();
     console.log(users);
 }
 
-try {
-    main();
-} catch (error) {
-    console.log("error running prisma test:", error);
-} finally (async() => ) {
-    await prisma.$disconnect();
-}
+main()
+    .catch((e) => { 
+        console.log("error running prisma tests", e);
+    })
+    .finally(async () => {
+        await prisma.$disconnect();
+    });
