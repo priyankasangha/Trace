d87a32da4be37a922c1c0960e32b734cfec69ce2
priyankasangha <priyasangha@hotmail.ca>
2025-10-11 18:45:54 -0700

    save local changes that update schema and userService

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,6 +1,8 @@
 import prisma from "../prisma/client.js";
 import { JourneyRole, JourneyVisibility } from "../prisma/client.js";
 
+
+// note: journeys friendships and friends of happens later
 export async function createUser(data) {
     if (!data.email || !data.name) {
         throw new Error("User must have a name and email");
@@ -10,13 +12,44 @@ export async function createUser(data) {
         data: {
             email: data.email,
             name: data.name,
-            profilePic: data.profilePic ?? null,
         },
     });
 
     return user;
 }
 
+
+export async function updateUserName(userId, newName) {
+    if (!newname || newName.trim() === "") {
+        throw new Error("Name cannot be empty");
+    }
+
+    return prisma.user.update({
+        where: { id: userId },
+        data: { name: newName},
+        include: { 
+            favourites: true,
+            friendships: true,
+            friendsOf: true, 
+        },
+    });
+}
+
+export async function updateProfilePic(userId, profilePicUrl) {
+    if (!profilePicUrl || profilePicUrl.trim() === "") {
+        throw new Error("Profile Pic URL can't be empty");
+    };
+
+    return await prisma.user.update({
+        where: { id: userId },
+        data: { profilePic: profilePicUrl },
+        include: {
+            favourites: true,
+            friendships: true,
+            friendsOf: true,
+        }
+    });
+}
 export async function getUserByEmail(email) {
     const user = await prisma.user.findUnique({
         where: { email },
@@ -40,6 +73,48 @@ export async function getUserById(id) {
 
     return user;
 }
+
+
+// FUNCTIONS FOR USER FAVOURITES
+export async function addUserFavourite(userId, journeyId) {
+    const updatedJourney = {
+        favourites: {
+            connect: { id: journeyId },
+        },
+    };
+
+    return await prisma.user.update({
+        where: { id: userId },
+        data: updatedJourney,
+        include: { favourites: true },
+    });
+}
+
+export async function removeUserFavourite(userId, journeyId) {
+    const updatedJourney = {
+        facourites: {
+            disconnect: { id: journeyId },
+        },
+    };
+
+    return prisma.user.update({
+        where: { id: userId },
+        data: updatedJourney,
+        include: { favourites: true},
+    });
+}
+
+export async function getUserFavourites(userId) {
+    return prisma.user.findUnique({
+        where: { id: userId },
+        include: { favourites: true },
+    });
+}
+
+
+
+
+
 
 // TO DO: UPDATE USER
 // export async function updateUser(id, date) {
